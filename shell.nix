10b4f1433f3d9e7e962e01d794eb7956d6574c43
Oskar Wickström <oskar@wickstrom.tech>
2019-06-27 08:20:22 +0200

    Use GHC 8.6.5

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {}, compiler ? "ghc862", doBenchmark ? true }:
+{ pkgs ? import <nixpkgs> {}, compiler ? "ghc865", doBenchmark ? true }:
 let
   haskellPackages = pkgs.haskell.packages.${compiler};
   project = import ./. { inherit compiler doBenchmark; };
