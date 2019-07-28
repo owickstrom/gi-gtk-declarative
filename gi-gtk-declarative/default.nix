0331e4b39444602e4defe49e0c1aaca596bf4f84
Oskar Wickström <oskar@wickstrom.tech>
2019-07-28 09:00:22 +0200

    Disable tests in Nix

--- gi-gtk-declarative/default.nix
+++ gi-gtk-declarative/default.nix
@@ -1,4 +1,6 @@
-{ pkgs ? import <nixpkgs> {}, compiler ? "ghc862", doBenchmark ? false }:
+{ pkgs ? import <nixpkgs> {}, compiler ? "ghc862"
+, doCheck ? false, doBenchmark ? false
+}:
 
 let
   haskellPackages = pkgs.haskell.packages.${compiler}.override {
@@ -8,8 +10,9 @@ let
       haskell-gi-overloading = pkgs.haskell.lib.dontHaddock (self.callHackage "haskell-gi-overloading" "1.0" {});
     };
   };
-  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
-  drv = variant (haskellPackages.callCabal2nix "gi-gtk-declarative" ./. {});
+  doCheck = if doCheck then pkgs.haskell.lib.doCheck else pkgs.lib.id;
+  doBench = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+  drv = doCheck (doBench (haskellPackages.callCabal2nix "gi-gtk-declarative" ./. {}));
 in
 {
   gi-gtk-declarative = drv;
