b20cedf4b49233c4c38585b8869b9cf06072b1b8
Gareth Daniel Smith <garethdanielsmith@gmail.com>
2020-04-18 15:13:50 +0100

    Fix the Nix/Travis app-simple build.

--- gi-gtk-declarative-app-simple/default.nix
+++ gi-gtk-declarative-app-simple/default.nix
@@ -3,13 +3,15 @@
 }:
 
 let
+  lib = import ../lib.nix { inherit pkgs; };
   haskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: {
       gi-gtk-declarative = gi-gtk-declarative;
     };
   };
   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
-  drv = variant (haskellPackages.callCabal2nix "gi-gtk-declarative-app-simple" ./. {});
+  drv = lib.checkWithGtkDeps
+    (variant (haskellPackages.callCabal2nix "gi-gtk-declarative-app-simple" ./. {}));
 in
 {
   gi-gtk-declarative-app-simple = drv;
