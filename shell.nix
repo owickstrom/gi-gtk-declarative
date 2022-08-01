0fd351bf9644daed03f1e46156d7bf6e7552ff36
Aaron VonderHaar <gruen0aermel@gmail.com>
2022-07-31 20:01:07 -0700

    Fix building docs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import ./nixpkgs.nix, compiler ? "ghc883", doBenchmark ? true, doCheck ? true }:
+{ pkgs ? import ./nixpkgs.nix, compiler ? "ghc884", doBenchmark ? true, doCheck ? true }:
 let
   fontsConf = pkgs.makeFontsConf {
     fontDirectories = [ pkgs.cantarell-fonts ];
@@ -14,8 +14,7 @@ in
       project.examples
     ];
     buildInputs = [
-      project.docs.packages.mkdocs
-      project.docs.packages.mkdocs-material
+      project.docs
       pkgs.cabal-install
     ];
     FONTCONFIG_FILE = fontsConf;
